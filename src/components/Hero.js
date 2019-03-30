12d2f28df32bea15c3a0665da3f8a1d8f31dad31
psikoi <ruben.amendoeira@gmail.com>
2019-03-30 23:16:09 +0000

    Add background image to banner

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 import './styles/Hero.css';
 
 export class Hero extends Component {
+
+  bannerStyle = {
+    background: 'url(' + process.env.PUBLIC_URL + '/img/banner_bg.png' + ')',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+  }
+
   render() {
     return (
-      <div className = "hero">
+      <div className="hero">
+        <div className="hero-container" style={this.bannerStyle}>
+        </div>
         <h1>
-            SOFTWARE<br/>
-            <span>DEVELOPMENT</span> & <span>DESIGN</span><br/>
-            PORTFOLIO
+          SOFTWARE<br />
+          <span>DEVELOPMENT</span> & <span>DESIGN</span><br />
+          PORTFOLIO
         </h1>
       </div>
     )
