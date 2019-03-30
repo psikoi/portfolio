b89554211b8008a0582b3c1b0e7acca778482084
psikoi <ruben.amendoeira@gmail.com>
2019-03-30 05:59:26 +0000

    Updates

---
+++ precache-manifest.e272b598850a6b4a106c3ec103060001.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "fa377282e2939c31d9cc",
+    "url": "/portfolio/static/js/main.34af226d.chunk.js"
+  },
+  {
+    "revision": "261ebb87132781baa326",
+    "url": "/portfolio/static/js/2.ca571872.chunk.js"
+  },
+  {
+    "revision": "fa377282e2939c31d9cc",
+    "url": "/portfolio/static/css/main.a4e0013b.chunk.css"
+  },
+  {
+    "revision": "bde41bc19b19d461507a72e02749e815",
+    "url": "/portfolio/index.html"
+  }
+];
